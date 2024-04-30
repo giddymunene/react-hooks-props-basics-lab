5b304c7f790b5d70904020c3262145753957bac0
gideon <gideonmunene0@gmail.com>
2024-04-30 10:37:53 -0400

    props

---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export function Links({ github, linkedin }) {
+  return (
+    <div>
+      <h3>Links</h3>
+      {github && <a href={github}>{github}</a>}
+      {linkedin && <a href={linkedin}>{linkedin}</a>}
+    </div>
+  );
+}
+
+
+export default Links;
